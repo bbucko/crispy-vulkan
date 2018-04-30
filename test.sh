15c283c147e72909ab8a56b259c31b3935eb1350
Blazej Bucko <blazej.bucko@gmail.com>
2018-04-30 11:05:48 +0200

    Added support for test results

--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 BUILD_DIR=build-$1
 cd $BUILD_DIR
 echo "Starting tests"
-make test
+mkdir -p $TEST_REPORTS
+echo "Saving tests results to $TEST_REPORTS"
+GTEST_OUTPUT="xml:$TEST_REPORTS/" make test
 echo "Tests done"
